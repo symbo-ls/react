052b40bd584ba40403cbdf29c4cff7c7a87a1738
Nika Otiashvili <nikaoto@gmail.com>
2023-06-07 16:46:19 +0400

    script fix

--- generate.sh
+++ generate.sh
@@ -2,9 +2,9 @@
 # Generate react components from smbls uikit
 
 GEN_DIR="src/uikit"
-SRC_DIR="domql-smbls-components"
+SRC_DIR="${1:-domql-smbls-components}"
 
-echo smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit "${@:1}"
+echo smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit "${@:2}"
 
 rimraf -I "$GEN_DIR" && \
-    smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit "${@:1}"
+    smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit "${@:2}"
