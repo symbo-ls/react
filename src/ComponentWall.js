073a6fedac4a6a2e37793ba03a154a7458bcf28f
Nika Otiashvili <nikaoto@gmail.com>
2023-07-21 16:42:57 +0400

    update test image, wrap wall with symbols provider

--- src/ComponentWall.js
+++ src/ComponentWall.js
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { SymbolsProvider } from "@symbo.ls/react";
+
 import { Brick, BrickTitle } from './Brick'
 import * as Atoms from './uikit/Atoms'
 import image from './test-img.jpg'
@@ -9,22 +11,24 @@ import svg from './bell.svg'
 export default function ComponentWall(props) {
 
   return (
-    <div>
-      <Brick title={"Atoms.Img"}>
-        <Atoms.Img
-          src={image}
-          alt={'Guy with long hair and wine in hand cheering'}
-          title={'Cheers!'} />
-      </Brick>
+    <SymbolsProvider>
+      <div>
+        <Brick title={"Atoms.Img"}>
+          <Atoms.Img
+            src={image}
+            alt={'Guy with long hair and wine in hand cheering'}
+            title={'Cheers!'} />
+        </Brick>
 
-      <Brick title={"Button"}>
-        <Button 
-          name={"my-button"}
-          icon={svg}
-          text={"Click me!"}
-          onClick={(ev) => console.log("aaaaaaa")} />
-      </Brick>
-
-    </div>
+        <Brick title={"Button"}>
+          <Button
+            icon={"arrow"}
+            theme={"primary"}
+            tag={"button"}
+            text={"Click me"}
+            onClick={(ev) => console.log("aaaaaaa")} />
+        </Brick>
+      </div>
+    </SymbolsProvider>
   )
 }
