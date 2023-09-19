d594763d209b1e9e8b81d54190557524b78782c3
Nika Otiashvili <nikaoto@gmail.com>
2023-09-19 11:32:27 +0400

    fix yarn linking errors

--- convert.sh
+++ convert.sh
@@ -2,11 +2,16 @@
 set -xe
 
 src="${1:-../smbls/uikit/domql/}"
+mrg="${2:-../smbls/uikit/react/}"
 dest="./src/uikit"
 tmp="./.smbls_convert_tmp/"
 
 rm -rf ./src/uikit
 
-smbls convert --internal-uikit -t "$tmp" "$src" "$dest"
-npx lerna exec -- yarn unlink && yarn link
+smbls convert "$src" "$dest" --internal-uikit -t "$tmp" -m "$mrg"
+
+set +e
+npx lerna exec --no-bail -- yarn unlink
+npx lerna exec --no-bail -- yarn link
+set -e
 npx lerna link --force-local
